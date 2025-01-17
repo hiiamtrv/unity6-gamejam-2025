c35afadf8fc4a750828dba693835a9561d2ac703
hiiamtrv <trungvo482@gmail.com>
2025-01-18 01:19:10 +0700

    Spikes

---
+++ Assets/Scripts/Dev/DevMove.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dev
+{
+    public class DevMove : MonoBehaviour
+    {
+        [SerializeField] private Vector3 offset;
+
+        private void Update()
+        {
+            transform.position += offset * Time.deltaTime;
+        }
+    }
+}
