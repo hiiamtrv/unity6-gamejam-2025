9c513c87375fd49f03d11528ba220ea992dafd21
hiiamtrv <trungvo482@gmail.com>
2025-01-18 01:42:25 +0700

    Bubble generator

---
+++ Assets/Scripts/Bubble/BubbleGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Bubble
+{
+    public class BubbleGenerator : MonoBehaviour
+    {
+        [SerializeField] private Vector2 generateIntervals;
+        [SerializeField] private GameObject prefabBubble;
+
+        private float timer;
+
+        private void Update()
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                GenBubble();
+                ResetTimer();
+            }
+        }
+
+        private void ResetTimer()
+        {
+            timer = Random.Range(generateIntervals.x, generateIntervals.y);
+        }
+
+        private void GenBubble()
+        {
+            var newBubble = Instantiate(prefabBubble, transform.position, Quaternion.identity);
+
+            //call bubble to pass bubble config
+            newBubble.SendMessage("SetBubbleConfig", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
--- Assets/Scripts/Dev/DevMove.cs
+++ Assets/Scripts/Dev/DevMove.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Dev
 {
     public class DevMove : MonoBehaviour
     {
         [SerializeField] private Vector3 offset;
+        [SerializeField] private bool randomMove;
+
+        private void Start()
+        {
+            if (randomMove)
+            {
+                offset = new Vector3(
+                    Random.Range(-5f, 5f),
+                    1f,
+                    0f
+                );
+            }
+        }
 
         private void Update()
         {
