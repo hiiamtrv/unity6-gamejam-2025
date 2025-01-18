9c39ec8724051b36688392cd7ce784673011b2cd
VuXuanHoangLam <blackkingofdead@gmail.com>
2025-01-18 10:48:02 +0700

    Redo Customer Class
    Change Customer Class into independent object that can countdown, choose it's colors, and leave when needed, Comment all CustomerManager for now

--- Assets/Scripts/Customer/Customer.cs
+++ Assets/Scripts/Customer/Customer.cs
@@ -1,31 +1,97 @@
-using TMPro;
-using Unity.VisualScripting;
+using GameManager;
 using UnityEngine;
-using UnityEngine.Events;
-
 public class Customer : MonoBehaviour
 {
-    private CustomerManager manager;
     [Header("Values")]
     [SerializeField] private int _wishColorIndex;
-    [SerializeField] private float _timer;
-    [SerializeField] private int _spawnIndex;
-
-    public void CustomerConfigure( int ColorIndex)
+    [SerializeField] private float _countdownTime;
+    [SerializeField] private float _minWaiting, _maxWaiting;
+    public void CustomerConfigure(int ColorIndex)
     {
         _wishColorIndex = ColorIndex;
     }
-    
+
     private void Awake()
     {
-        manager = FindFirstObjectByType<CustomerManager>();
+        _minWaiting = 3f;
+        _maxWaiting = 6f;
+        _countdownTime = Random.Range(_minWaiting, _maxWaiting);
     }
-    public void AddIndex(int Index)
+    private void OnEnable()
     {
-        _spawnIndex=Index;
+        _wishColorIndex = RandomFavouriteColorIndex();
+    }
+    private int RandomFavouriteColorIndex()
+    {
+        return Random.Range(0, 17);
+    }
+    private void Waiting()
+    {
+        if (_countdownTime > 0)
+        {
+            _countdownTime -= Time.deltaTime;
+            Debug.Log("I'm waiting");
+        }
+        else
+        {
+            Debug.Log("Done waiting, leaving");
+            Leave();
+        }
+    }
+    private void Satisfied()
+    {
+        //Show satisfaction
+        Debug.Log("I like this");
+    }
+    private void Angry()
+    {
+        //Show angry face
+        Debug.Log("I dont like this");
+        //Deduce reputation
+        GameManager gameManager = FindObjectsByType<GameManager>(FindObjectsSortMode.None);
+        if (gameManager != null)
+        {
+            gameManager.AddReputation(10); // Add 10 to reputation
+            Debug.Log("Added 10 reputation.");
+        }
+    }
+    private void ShowingWishColor()
+    {
+        //Change ColorIndex to real color
+        Debug.Log("My favourite color is" + _wishColorIndex.ToString());
+        //Delay;
     }
     private void Update()
     {
+        //CustomerAppear
+        ShowingWishColor();
+        //Waiting
+        Invoke("Waiting", 1f);
+        //DoneWaiting, leave
+    }
+    private void Leave() {
+        Debug.Log("Left...");
+        Destroy(gameObject, 1f);
+
+    }
+    private void OnCollisionEnter2D(Collision2D coli2D)
+    {
+        if (coli2D.collider.CompareTag("Bubble"))
+        {
+            int bubbleColor = 4;
+            //bubbleColor = coli2D.collider.TryGetComponent<Bubble>().GetColorIndex();
+            //check mau == _wishColorIndex thi huy customer
+            if (bubbleColor == _wishColorIndex)
+            {
+                Satisfied();
+                Leave();
+            }
+            else //check mau != _wishColorIndex thi goi GameManager - mau va huy customer
+            {
+                Angry();
+            }
+            
+        }
 
     }
 }
